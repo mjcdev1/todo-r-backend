4a097053c64ba075cb13018f312a8a2708f8b802
mjcdev1 <146114111+mjcdev1@users.noreply.github.com>
2023-12-12 15:28:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,31 @@
-import os
+import os, requests
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
 
 app = Flask(__name__)
-CORS(app)
-
-# MongoDB Atlas connection string
+CORS(app, origins=['https://todo.mjc-dev.com'])
 mongo_uri = "mongodb+srv://mjcdeveloper1:SmokingNarhwal226$@todo-r-cluster.nrtn6of.mongodb.net/?retryWrites=true&w=majority"
+
 client = MongoClient(mongo_uri)
-data = client.data 
-lists = data.lists  # Move this outside of the route to avoid repeated database connections
 
-@app.route('/create_list', methods=['POST', 'OPTIONS'])
-def create_list():
+db = client.data  
+
+@app.route('/create_user', methods=['POST'])
+def receive_data():
     try:
-        received_data = request.get_json()
-
-        if request.method == 'OPTIONS':
-            # Handling CORS pre-flight request
-            response = jsonify({'message': 'CORS pre-flight request successful'})
-            response.headers['Access-Control-Allow-Origin'] = 'https://todo.mjc-dev.com'
-            response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-            response.headers['Access-Control-Allow-Methods'] = 'POST'
-            return response
-
-        lists.insert_one(received_data)
-        processed_data = "Received data: " + str(received_data)
-
-        # Manual CORS headers for the actual request
-        response = jsonify({"message": processed_data})
-        response.headers['Access-Control-Allow-Origin'] = 'https://todo.mjc-dev.com'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        response.headers['Access-Control-Allow-Methods'] = 'POST'
+        data = request.get_json()
         
-        return response, 200
+        lists = db.lists
+
+        inserted_data = lists.insert_one(data)
+        
+        processed_data = "Received data: " + str(data)
+        
+        return processed_data, 200
     except Exception as e:
         return str(e), 500
 
-if __name__ == "__main__":
-    # For local development
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
