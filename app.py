a964c73d8c76a9a694aca0dd80f3f1f710913b6d
mjcdev1 <146114111+mjcdev1@users.noreply.github.com>
2023-12-12 15:14:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ from flask_cors import CORS
 from pymongo import MongoClient
 
 app = Flask(__name__)
-from flask_cors import CORS
-
-CORS(app, origins=['https://todo.mjc-dev.com'])
-
+CORS(app)
 
 # MongoDB Atlas connection string
 mongo_uri = "mongodb+srv://mjcdeveloper1:SmokingNarhwal226$@todo-r-cluster.nrtn6of.mongodb.net/?retryWrites=true&w=majority"
@@ -15,17 +12,32 @@ client = MongoClient(mongo_uri)
 data = client.data 
 lists = data.lists  # Move this outside of the route to avoid repeated database connections
 
-@app.route('/create_list', methods=['POST'])
+@app.route('/create_list', methods=['POST', 'OPTIONS'])
 def create_list():
     try:
         received_data = request.get_json()
-        inserted_data = lists.insert_one(received_data)  # Fix this line to insert received_data, not data
+
+        if request.method == 'OPTIONS':
+            # Handling CORS pre-flight request
+            response = jsonify({'message': 'CORS pre-flight request successful'})
+            response.headers['Access-Control-Allow-Origin'] = 'https://todo.mjc-dev.com'
+            response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+            response.headers['Access-Control-Allow-Methods'] = 'POST'
+            return response
+
+        lists.insert_one(received_data)
         processed_data = "Received data: " + str(received_data)
-        
-        # Add CORS headers to the response
+
+        # Manual CORS headers for the actual request
         response = jsonify({"message": processed_data})
-        response.headers.add("Access-Control-Allow-Origin", "https://todo.mjc-dev.com")
+        response.headers['Access-Control-Allow-Origin'] = 'https://todo.mjc-dev.com'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        response.headers['Access-Control-Allow-Methods'] = 'POST'
         
         return response, 200
     except Exception as e:
         return str(e), 500
+
+if __name__ == "__main__":
+    # For local development
+    app.run(debug=True)
