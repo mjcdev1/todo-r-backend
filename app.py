eac20a37013be6edb4eef08bab7976fdab88a12a
mjcdev1 <146114111+mjcdev1@users.noreply.github.com>
2023-12-12 15:39:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
-CORS(app, origins=['https://todo.mjc-dev.com'], methods=['POST'], supports_credentials=True)  # Specify CORS configuration
+CORS(app, origins=['https://todo.mjc-dev.com'], methods=['POST'], supports_credentials=True)
 
 # MongoDB connection setup
 mongo_uri = "mongodb+srv://mjcdeveloper1:SmokingNarhwal226$@todo-r-cluster.nrtn6of.mongodb.net/?retryWrites=true&w=majority"
@@ -14,16 +14,6 @@ try:
     print("Connected to MongoDB successfully!")
 except Exception as e:
     print(f"Error connecting to MongoDB: {e}")
-
-# Handle CORS headers for OPTIONS requests
-@app.route('/create_user', methods=['OPTIONS'])
-def handle_options():
-    response = jsonify({'message': 'CORS preflight successful'})
-    response.headers['Access-Control-Allow-Origin'] = 'https://todo.mjc-dev.com'
-    response.headers['Access-Control-Allow-Methods'] = 'POST'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    response.headers['Access-Control-Max-Age'] = '3600'  # Cache preflight result for 1 hour
-    return response
 
 # Your main route for handling POST requests
 @app.route('/create_user', methods=['POST'])
@@ -40,3 +30,4 @@ def receive_data():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, threaded=True)
+
