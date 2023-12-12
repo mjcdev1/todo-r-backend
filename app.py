0b7a20e9ddc1c57176e3a052e73318fc9a9da1d8
mjcdev1 <146114111+mjcdev1@users.noreply.github.com>
2023-12-12 15:00:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os, requests
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
@@ -9,20 +9,20 @@ CORS(app, origins=['https://todo.mjc-dev.com'])
 # MongoDB Atlas connection string
 mongo_uri = "mongodb+srv://mjcdeveloper1:SmokingNarhwal226$@todo-r-cluster.nrtn6of.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(mongo_uri)
-
 data = client.data 
+lists = data.lists  # Move this outside of the route to avoid repeated database connections
 
 @app.route('/create_list', methods=['POST'])
 def create_list():
     try:
         received_data = request.get_json()
-
-        lists = data.lists
-
-        inserted_data = lists.insert_one(data)
-        
+        inserted_data = lists.insert_one(received_data)  # Fix this line to insert received_data, not data
         processed_data = "Received data: " + str(received_data)
         
-        return processed_data, 200
+        # Add CORS headers to the response
+        response = jsonify({"message": processed_data})
+        response.headers.add("Access-Control-Allow-Origin", "https://todo.mjc-dev.com")
+        
+        return response, 200
     except Exception as e:
         return str(e), 500
