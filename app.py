e16e25b9a794a8ba1a0c50d09e73844da372f7c8
mjcdev1 <146114111+mjcdev1@users.noreply.github.com>
2023-12-13 13:36:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,40 @@
-from flask import Flask, request, jsonify, make_response
-from flask_cors import CORS, cross_origin
+import os, requests
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from pymongo import MongoClient
-import os
 
 app = Flask(__name__)
+CORS(app, origins=['https://todo.mjc-dev.com'])
 
+# MongoDB Atlas connection string
 mongo_uri = "mongodb+srv://mjcdeveloper1:NecroticUvula@todo-r-cluster.nrtn6of.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a MongoClient instance
 client = MongoClient(mongo_uri)
-db = client.data
 
-@app.route('/create_user', methods=['POST', 'OPTIONS'])
-@cross_origin(origin='https://todo.mjc-dev.com', methods=['POST'], headers=['Content-Type'])
+db = client.data  
+
+@app.route('/create_user', methods=['POST'])
 def receive_data():
-    if request.method == 'OPTIONS':
-        response = make_response()
-        return response
-
     try:
         data = request.get_json()
+        
         lists = db.lists
+
         inserted_data = lists.insert_one(data)
+        
         processed_data = "Received data: " + str(data)
-
-        response = make_response(processed_data, 200)
-        return response
-
+        
+        return processed_data, 200
     except Exception as e:
         return str(e), 500
 
+    return jsonify(response)
+
+
+
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, threaded=True)
-
+    app.run(host="0.0.0.0", port=port)
