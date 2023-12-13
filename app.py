328cf8dc15145f3807333a360ae4edc17279798d
mjcdev1 <146114111+mjcdev1@users.noreply.github.com>
2023-12-13 17:31:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["http://10.0.0.187:8080/"])
+CORS(app, origins=["http://10.0.0.187:8080"])
 
 @app.route('/ping', methods=['POST'])
 def receive_ping():
     if request.method == 'POST':
         try:
             data = request.get_json()
-
             print("Received data:", data)
             response_data = {'message': 'ping'}
             return jsonify(response_data), 200
