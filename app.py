5f1dfd4d43bd70e4ded1f94aeba5d947e8fee897
mjcdev1 <146114111+mjcdev1@users.noreply.github.com>
2023-12-13 17:28:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,11 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/ping": {"origins": "*"}})
+CORS(app, origins=["https://specific-origin.com"])
 
-@app.route('/ping', methods=['POST', 'OPTIONS'])
+@app.route('/ping', methods=['POST'])
 def receive_ping():
-    if request.method == 'OPTIONS':
-        headers = {
-            'Access-Control-Allow-Methods': 'POST, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Max-Age': 86400  # 24 hours
-        }
-        return '', 204, headers
-
-    elif request.method == 'POST':
+    if request.method == 'POST':
         try:
             data = request.get_json()
 
@@ -23,11 +15,9 @@ def receive_ping():
             return jsonify(response_data), 200
 
         except Exception as e:
-            # Handle any exceptions or errors
             print("Error:", str(e))
             return jsonify({'message': 'Internal server error'}), 500
     else:
-        # If the request method is not POST or OPTIONS
         return jsonify({'message': 'Method not allowed'}), 405
 
 if __name__ == '__main__':
